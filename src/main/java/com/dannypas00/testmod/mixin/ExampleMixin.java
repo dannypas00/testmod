9c5201b3ea98feb8c44907c7d9591b45342bbe2c
dannypas00 <dannypas00@outlook.com>
2022-01-08 19:44:16 +0100

    Inital setup, stuff compiles now

--- src/main/java/com/dannypas00/testmod/mixin/ExampleMixin.java
+++ src/main/java/com/dannypas00/testmod/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package com.dannypas00.testmod.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import com.dannypas00.testmod.TestMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		TestMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
